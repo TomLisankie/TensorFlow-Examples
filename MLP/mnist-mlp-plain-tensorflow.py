91619afbbb0bd0fdc6c5aa6d0f2043d14177004b
Thomas Lisankie <thomas.lisankie@gmail.com>
2018-06-17 16:43:17 -0400

    Finished MNIST MLP

--- MLP/mnist-mlp-plain-tensorflow.py
+++ MLP/mnist-mlp-plain-tensorflow.py
@@ -27,10 +27,10 @@ def neuron_layer(X, n_neurons, layer_name, activation = None):
             return Z 
         
 with tf.name_scope("dnn"):
-    hidden1 = neuron_layer(X, n_hidden1, name = "hidden1", activation=tf.nn.relu)
-    hidden2 = neuron_layer(hidden1, n_hidden2, name = "hidden2", activation = tf.nn.relu)
+    hidden1 = neuron_layer(X, n_hidden1, layer_name = "hidden1", activation=tf.nn.relu)
+    hidden2 = neuron_layer(hidden1, n_hidden2, layer_name = "hidden2", activation = tf.nn.relu)
     # logits are the output of the network before going through softmax
-    logits = neuron_layer(hidden2, n_outputs, name = "outputs") 
+    logits = neuron_layer(hidden2, n_outputs, layer_name = "outputs") 
 
 with tf.name_scope("loss"):
     cross_entropy = tf.nn.sparse_softmax_cross_entropy_with_logits(labels = y, logits = logits)
@@ -51,3 +51,22 @@ Execution Phase
 '''
 init = tf.global_variables_initializer()
 saver = tf.train.Saver()
+
+from tensorflow.examples.tutorials.mnist import input_data
+mnist = input_data.read_data_sets("/tmp/data")
+
+n_epochs = 400
+batch_size = 50
+
+with tf.Session() as sess:
+    init.run()
+    for epoch in range(n_epochs):
+        for iteration in range(mnist.train.num_examples // batch_size):
+            X_batch, y_batch = mnist.train.next_batch(batch_size)
+            sess.run(training_op, feed_dict = {X: X_batch, y: y_batch})
+        acc_train = accuracy.eval(feed_dict = {X: X_batch, y: y_batch})
+        acc_test = accuracy.eval(feed_dict = {X: mnist.test.images, y: mnist.test.labels})
+        
+        print(epoch, "Train accuracy:", acc_train, "Test accuracy:", acc_test)
+    
+    save_path = saver.save(sess, "./mnist_model_final.ckpt")
