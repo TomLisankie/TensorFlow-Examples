df1721432abca131fdc861ecb388cb9bb6271ef0
Thomas Lisankie <thomas.lisankie@gmail.com>
2018-06-20 14:31:52 -0400

    Added some comments for each of the variables.

--- RNNs/toy_rnn_plain.py
+++ RNNs/toy_rnn_plain.py
@@ -8,13 +8,14 @@ timesteps = 100 # number of elements per sequence
 input_features = 32 # number of features for each sequence element vector
 output_features = 64 # number of features for each output vector
 
+# random noise to use as input data. Gets multiplied with the input weights
 inputs = np.random.random((timesteps, input_features))
 
-W = np.random.random((output_features, input_features))
-U = np.random.random((output_features, output_features))
-b = np.random.random((output_features))
+W = np.random.random((output_features, input_features)) # input weights
+U = np.random.random((output_features, output_features)) # recurrent weights
+b = np.random.random((output_features)) # bias vector
 
-state_t = np.zeros((output_features))
+state_t = np.zeros((output_features)) # state to be multiplied with the recurrent weights
 
 successive_outputs = []
 for input_t in inputs:
